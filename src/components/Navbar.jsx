08ff2d26151ffab2692abc49e56f8a8b4f112f09
Rahil Patel <Rahil.Patel1715@gmail.com>
2023-12-29 01:26:40 -0500

    Added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Stack } from "@mui/material"
 import { Link } from "react-router-dom"
-import { logo } from '../utils/constants'
+import { logo } from "../assets"
 
 import SearchBar from "./SearchBar"
 
